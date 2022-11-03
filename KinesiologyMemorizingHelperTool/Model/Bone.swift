01db9bdd128e544aa268b55f0733e63d3088e04c
Nathan Smith <nsmith@lcs.on.ca>
2022-11-03 16:10:44 -0400

    photos import for bones done, model done, added remote image view, and started populating the list

---
+++ KinesiologyMemorizingHelperTool/Model/Bone.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  KinesiologyMemorizingHelperTool
+//
+//  Created by Nathan on 2022-11-03.
+//
+
+import Foundation
+
+struct Bone {
+    let id = UUID()
+    let imageName: String
+}
--- KinesiologyMemorizingHelperTool/Views/MainPageView.swift
+++ KinesiologyMemorizingHelperTool/Views/MainPageView.swift
@@ -10,6 +10,90 @@ import SwiftUI
 struct MainPageView: View {
     // MARK: Stored Properties
     
+    var listOfPictures: [Bone] = [
+        
+        Bone(imageName: "Atlas")
+        ,
+        
+        Bone(imageName: "Axis")
+        
+        ,
+        
+        Bone(imageName: "Calcaneus")
+        ,
+        
+        Bone(imageName: "Cervical Spine")
+        
+        ,
+        
+        Bone(imageName: "Clavicle")
+        ,
+        
+        Bone(imageName: "Coccyx")
+        ,
+        
+        Bone(imageName: "Costal Cartilage")
+        ,
+        
+        Bone(imageName: "femur")
+        
+        ,
+        
+        Bone(imageName: "Fibula")
+        ,
+        
+        Bone(imageName: "Frontal Bone")
+        
+        ,
+        
+        Bone(imageName: "Humerus")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+        
+        ,
+        
+        Bone(imageName: "")
+    ]
+    
+    
     // MARK: Computed Properties
     var body: some View {
         ZStack {
@@ -20,7 +104,8 @@ struct MainPageView: View {
                 Text("Nathan's Kinesiology Memorizing Helper Tool")
                     .font(.system(size: 35, weight: .medium, design: .serif))
                 HStack {
-                    Image("femur")
+                    let rand = Int.random(in: 0..<listOfPictures.count)
+                    Image(listOfPictures[rand].imageName)
                         .resizable()
                         .scaledToFit()
                     Image("patella")
