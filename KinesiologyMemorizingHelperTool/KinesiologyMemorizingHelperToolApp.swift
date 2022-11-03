b7ec4b490a24fb1fbd161e43801dfecf07366630
Nathan Smith <nsmith@lcs.on.ca>
2022-11-02 20:26:53 -0400

    Initial Commit

---
+++ KinesiologyMemorizingHelperTool/KinesiologyMemorizingHelperToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinesiologyMemorizingHelperToolApp.swift
+//  KinesiologyMemorizingHelperTool
+//
+//  Created by Nathan on 2022-11-02.
+//
+
+import SwiftUI
+
+@main
+struct KinesiologyMemorizingHelperToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
